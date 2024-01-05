9a2960b90e8bd815b72cb2c76e9a045d6ba11de7
wuhuiluo666 <510426343@qq.com>
2024-01-05 12:19:52 +0800

    commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const cors = require('koa2-cors')
 const Koa = require('koa')
 const Router = require('koa-router')
-const Cors = require('cors')
 
 const app = new Koa()
+
+
+app.use(cors({
+  origin:"*", // 允许所有域名请求
+  maxAge: 5, // 本次预检请求的有效期，单位为秒。
+  methods:['GET','POST'],  // 所允许的HTTP请求方法
+  alloweHeaders:['Conten-Type'], // 服务器支持的所有头信息字段
+  credentials: true // 是否允许发送Cookie
+}))
+
+
 const router = new Router()
 
 const mockList = require('./mock/index')
@@ -26,6 +37,5 @@ mockList.forEach((mock) => {
   })
 })
 
-app.use(Cors())
 app.use(router.routes())
 app.listen(3001)
